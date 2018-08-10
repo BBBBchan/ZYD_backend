b1b8d40ae9d75f7d0c44abe93561bec79700c113
BBchan <sbysbysby123@gmail.com>
2018-08-10 18:59:23 +0800

    返回信息调整

--- app/comment/views.py
+++ app/comment/views.py
@@ -11,17 +11,18 @@ from app.config import *
 def comment_list():
 	data = request.json
 	user_id = data.get('user_id')
+	if user_id == None:
+		return jsonify({'message':'获取用户id失败'}),400
 	pic_id = data.get('pic_id')
+	if pic_id == None:
+		return jsonify({'message':'获取图片id失败'}),401
 	page_count = data.get('page_count', 10)
 	page_num = data.get('page_num', 1)
-	
-	if pic_id == None or user_id == None:							
-		return jsonify({'message':'用户或图片信息不能为空'}), 400
 	try:
 		comment_all = CommentPicture.query.filter_by(content_id = pic_id).all()
 	except:
 		db.session.rollback()
-		return jsonify({'message':'获取评论失败'}),401
+		return jsonify({'message':'获取评论失败'}),402
 	res = {}
 	for i in range(page_count*(page_num-1),page_count*page_num):
 		comment = {
@@ -40,14 +41,22 @@ def comment_list():
 def comment_modify():
 	data = request.json
 	comment_id = data.get('comment_id')
+	if comment_id == None:
+		return jsonify({'message':'获取评论id失败'}),400
 	user_id = data.get('user_id')
+	if user_id == None:
+		return jsonify({'message':'获取用户id失败'}),401
 	pic_id = data.get('pic_id')
+	if pic_id == None:
+		return jsonify({'message':'获取图片id失败'}),402
 	comment_detail = data.get('comment_detail')
+	if comment_detail == None:
+		return jsonify({'message':'获取评论内容失败'}),403
 
 	try:	
 		comment = CommentPicture.query.filter_by(id=comment_id,content_id=pic_id,commentator_id=user_id).first()
 		if comment.context == comment_detail:
-			return jsonify({'message':'评论内容未修改'}),401
+			return jsonify({'message':'评论内容未修改'}),405
 		else:
 			comment.context = comment_detail
 			try:
@@ -55,10 +64,10 @@ def comment_modify():
 				return jsonify({'message':'修改评论成功'})
 			except:
 				dn.session.rollback()
-				return jsonify({'message':'修改评论失败'})
+				return jsonify({'message':'修改评论失败'}),406
 	except:
 		db.session.rollback()
-		return jsonify({'message':'获取评论失败'}),400
+		return jsonify({'message':'获取评论失败'}),404
 
 #删除评论
 @comment_blueprint.route('/comment_delete', methods=['GET','POST'])						
@@ -66,8 +75,14 @@ def comment_modify():
 def comment_delete():
 	data = request.json
 	comment_id = data.get('comment_id')
+	if comment_id == None:
+		return jsonify({'message':'获取评论id失败'}),400
 	user_id = data.get('user_id')
+	if user_id == None:
+		return jsonify({'message':'获取用户id失败'}),401
 	pic_id = data.get('pic_id')
+	if pic_id == None:
+		return jsonify({'message':'获取图片id失败'}),402
 
 	try:	
 		comment = CommentPicture.query.filter_by(id=comment_id, content_id=pic_id, commentator_id=user_id).first()
@@ -77,10 +92,10 @@ def comment_delete():
 			return jsonify({'message':'删除成功'})
 		except:
 			db.session.rollback()
-			return jsonify({'message':'删除失败'}),400
+			return jsonify({'message':'删除失败'}),404
 	except:
 		db.session.rollback()
-		return jsonify({'message':'获取评论失败'}),401
+		return jsonify({'message':'获取评论失败'}),403
 
 #添加评论
 @comment_blueprint.route('/comment_upload', methods=['POST'])							
@@ -90,13 +105,13 @@ def comment_upload():
 	user_id = data.get('user_id')
 	
 	if user_id == None:
-		return jsonify({'message':'用户名不能为空'}),400
+		return jsonify({'message':'获取用户id失败'}),400
 	pic_id = data.get('pic_id')
 	if pic_id == None:
-		return jsonify({'message':'未找到相应图片'}),401
+		return jsonify({'message':'获取图片id失败'}),401
 	comment_detail = data.get('comment_detail')
 	if comment_detail == None:
-		return jsonify({'message':'评论内容不能为空'}),402	
+		return jsonify({'message':'获取评论内容失败'}),402	
 	comment_time = str(datetime.now())
 	
 	try:
