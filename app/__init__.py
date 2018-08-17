51840313644acb9e91863e3cd4179c48fab3d0e1
oneSaber <1357860084@qq.com>
2018-08-17 18:35:35 +0800

    增加标签控制

--- app/__init__.py
+++ app/__init__.py
@@ -13,6 +13,7 @@ from .picture import picture_blueprint
 from .showCase.views import showcase_blueprint
 from .user.views import user_blueprint
 from .video import video_blueprint
+from .Picture_manage import pictrue_manage_blueprint
 
 
 def create_app(object_name):
@@ -28,6 +29,7 @@ def create_app(object_name):
     app.register_blueprint(order_blueprint, url_prefix='/api/order')
     app.register_blueprint(comment_blueprint, url_prefix='/api/comment')
     app.register_blueprint(admin_blueprint, url_prefix='/api/admin')
+    app.register_blueprint(pictrue_manage_blueprint, url_prefix='/api/picture_manage')
 
     return app
 
